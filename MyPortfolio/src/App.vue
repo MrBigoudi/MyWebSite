84e4288e0bb3c801cd3bf377111b606dae710d15
MrBigoudi <yannis.kedadry@ens.psl.eu>
2023-05-07 09:44:42 +0200

    init vuejs

---
+++ MyPortfolio/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import Home from "./components/Home.vue"
+  import NavBar from "./components/NavBar.vue"
+</script>
+
+<template>
+  <header>
+    <div>
+      <NavBar />
+    </div>
+  </header>
+
+  <main>
+    <Home />
+  </main>
+</template>
---
+++ MyPortfolio/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>NavBar</h1>>
+</template>
