860e916dbdc29c7a86aaba3f200d0c890a406fd3
MrBigoudi <yannis.kedadry@ens.psl.eu>
2023-05-10 23:56:36 +0200

    trying to make the navbar responsive

--- MyPortfolio/src/components/Navbar.vue
+++ MyPortfolio/src/components/Navbar.vue
@@ -3,34 +3,59 @@
   export default {
     data() {
       return {
+        showMenu: false,
       }
     },
     components:{
       Icon
+    },
+    methods: {
+      toggleMenu() {
+        this.showMenu = !this.showMenu;
+        // console.log(this.showMenu);
+      }
     }
   }
 </script>
 
 <template>
-  <header id="navbar">
-    <div className="flex justify-around pt-3 pb-3 font-roboto font-bold text-2xl items-center
-        fixed top-0 left-0 right-0 z-50 backdrop-filter backdrop-blur-lg">
-
-      <div class="logo" className="ml-10 mr-auto text-dark-coral font-bold italic">
-        <a href="#home" className="scroll-smooth transition ease-in-out duration-1000">
-          Yannis.K
-        </a>
-      </div>
-
-      <nav className="flex justify-end gap-12 mr-24">
-        <Icon name="Home" ref="#home" icon="home"/>
-        <Icon name="About" ref="#about" icon="person"/>
-        <Icon name="Projects" ref="#projects" icon="storage"/>
-        <Icon name="Resume" ref="#resume" icon="description"/>
-        <Icon name="Contact" ref="#contact" icon="mark_as_unread"/>
-      </nav>
+  <header class="flex flex-col md:flex-row items-center
+    font-roboto h-16 font-bold text-2xl 
+    fixed top-0 left-0 right-0 z-50 
+    backdrop-filter backdrop-blur-lg">
     
+    <div class="ml-10 text-dark-coral font-bold italic hidden md:block">
+      <a href="#home" class="scroll-smooth transition ease-in-out duration-1000">
+        Yannis.K
+      </a>
     </div>
+
+    <nav class="hidden md:flex md:flex-row justify-end gap-0 mr-0 h-full w-full">
+      <Icon name="Home" link="#home" icon="home" class="ml-auto" focusAuto="true"/>
+      <Icon name="About" link="#about" icon="person"/>
+      <Icon name="Projects" link="#projects" icon="storage"/>
+      <Icon name="Resume" link="#resume" icon="description"/>
+      <Icon name="Contact" link="#contact" icon="mark_as_unread"/>
+    </nav>
+
+    <button @click="toggleMenu()" class="md:hidden ml-auto mr-4 h-full text-center text-dark-coral
+      flex justify-end scroll-smooth h-full items-center px-4 focus:outline-none" >
+      <i class="material-icons text-6xl">menu</i>
+    </button>
+
+    <nav v-if="showMenu" class="flex flex-col justify-evenly items-center gap-2 w-full 
+      text-center z-1" :class="{'backdrop-filter backdrop-blur-lg': showMenu}">
+      <Icon name="Home" link="#home" icon="home" 
+        class="w-full px-0"/>
+      <Icon name="About" link="#about" icon="person"
+        class="w-full px-0"/>
+      <Icon name="Projects" link="#projects" icon="storage"
+        class="w-full px-0"/>
+      <Icon name="Resume" link="#resume" icon="description"
+        class="w-full px-0"/>
+      <Icon name="Contact" link="#contact" icon="mark_as_unread"
+        class="w-full px-0"/>
+    </nav>
 
   </header>
 </template>
--- MyPortfolio/src/components/icons/Icon.vue
+++ MyPortfolio/src/components/icons/Icon.vue
@@ -1,16 +1,41 @@
 <script>
   export default {
+    data(){
+      return {
+      }
+    },
     props:{
       name: "",
-      ref: "",
+      link: "",
       icon: "",
-    }
+      focusAuto: false,
+    },
+    mounted() {
+        // console.log("Name prop:", this.name);
+        // console.log("Icon prop:", this.icon);
+        // console.log("Ref prop:", this.link);
+
+        // autofocus
+        if(this.focusAuto){
+          // console.log("#" + this.name);
+          const element = document.querySelector("#" + this.name);
+          element.setAttribute('tabindex', '0');
+          element.focus();
+        }
+    },
+    methods: {
+      goTo(){
+        const targetElement = document.querySelector(this.link);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      },
+    },
   }
 </script>
 
 <template>
-  <a :href="icon" className="flex justify-evenly scroll-smooth transition ease-in-out duration-1000">
+  <a :id="name" :href="link" @click.prevent="goTo" class="flex justify-center scroll-smooth h-full items-center px-4 
+    focus:outline-none active:bg-dark-coral focus:bg-dark-coral">
     <i class="material-icons">{{ icon }}</i>
-    <p>{{ name }}</p>
+    <p class="px-2">{{ name }}</p>
   </a>
 </template>
