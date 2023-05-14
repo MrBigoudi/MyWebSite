19f40a63b6d8803a09bca52ec3325065950d8ece
MrBigoudi <yannis.kedadry@ens.psl.eu>
2023-05-14 22:43:58 +0200

    trying to add resume

--- MyPortfolio/src/components/Resume.vue
+++ MyPortfolio/src/components/Resume.vue
@@ -1,14 +1,36 @@
 <script>
-export default {
-  data() {
-    return {
+  import pdfjs from 'pdfjs-dist'
+
+  export default {
+    mounted() {
+      const container = this.$refs.pdfViewer
+      const url = '@/assets/pdfs/resume.pdf'
+      
+      pdfjs.getDocument(url).promise.then(pdf => {
+        pdf.getPage(1).then(page => {
+          const scale = 1.5
+          const viewport = page.getViewport({scale: scale})
+          const canvas = document.createElement('canvas')
+          const context = canvas.getContext('2d')
+          canvas.height = viewport.height
+          canvas.width = viewport.width
+
+          const renderContext = {
+            canvasContext: context,
+            viewport: viewport
+          }
+
+          page.render(renderContext).promise.then(() => {
+            container.appendChild(canvas)
+          })
+        })
+      })
     }
   }
-}
 </script>
 
 <template>
   <section id="resume">
-    <h1>Resume</h1>>
+    <div ref="pdfViewer"></div>
   </section>
 </template>
