2197fba9bde5d35ebb518c45a96eb2cbcf70707e
MrBigoudi <yannis.kedadry@ens.psl.eu>
2023-05-23 22:04:39 +0200

    fixing navbar ref issues

--- MyPortfolio/src/components/utils/projects/Project.vue
+++ MyPortfolio/src/components/utils/projects/Project.vue
@@ -22,14 +22,14 @@
 
 <template>
     <a :href="link" class="flex flex-col items-center px-0.5 text-center
-        w-1/3 h-128 pt-4 hover:font-bold hover:border-white border-2 border-transparent
+        w-1/3 h-128 pt-4 hover:font-bold hover:border-white border-2 border-transparent group
         "> <!-- project container -->
         <div class="w-full h-2/3 px-1"> <!-- project image -->
             <img :src="image?image:`/src/assets/projects/projectDefault.png`" :alt="alter" />
         </div>
 
         <div class="flex flex-col items-center text-center h-full mb-4">
-             <div class="text-4xl font-bold mt-6"> <!-- project title -->
+             <div class="text-3xl group-hover:text-4xl font-bold mt-6"> <!-- project title -->
                 {{ name }}
             </div>
             <div class="text-2xl mt-3 pb-8"> <!-- project description -->
